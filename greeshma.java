3af71329a9b2b51ec93e5c213a2e483a2939494a
Gopi <haigopi@gmail.com>
2019-05-16 12:08:14 -0500

    variable

---
+++ greeshma.java
@@ -0,0 +1,14 @@
+package geeta;
+
+public class greeshma {
+public static void main(String[]ary){
+	int a=10;
+			String name = "xyz";
+			float b=10;
+			double c=30.234;
+			System.out.println("int a="+a);
+	System.out.println("float b="+c);
+	System.out.println("double c="+c);
+			
+}
+}
