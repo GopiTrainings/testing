1ed33cb5274842d50836910d00fdd381e6b27cf8
Vasuvasu <vjla123@yahoo.com>
2019-05-16 12:45:13 -0500

    variable

---
+++ vijaya14.java
@@ -0,0 +1,22 @@
+package Vijaya;
+
+public class vijaya14 {
+
+	public static void main(String[] args) {
+	
+		
+		int a=10;
+		String myname = "Vijaya";
+		
+		
+		float b=11;
+		double c=21.234;
+		System.out.println("int a=" +a);
+		System.out.println("float b=" +b);
+		System.out.println("double c=" +c);
+		System.out.println("String myname =" +myname);
+		// TODO Auto-generated method stub
+
+	}
+
+}
