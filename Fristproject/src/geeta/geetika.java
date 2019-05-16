0e3cdff1d14bc6e5a7612b4a70946c417dc028e6
Gopi <haigopi@gmail.com>
2019-05-16 12:59:08 -0500

    2 VARIABLE

---
+++ Fristproject/src/geeta/geetika.java
@@ -0,0 +1,11 @@
+package geeta;
+
+public class geetika {
+public static void main(String[]ary){
+int a =20;
+String ABC = "XYZ";
+System.out.println("inta="+a);
+System.out.println("String ABC =" + ABC);
+}
+		
+}
